5e06a2eab13b9c7d5909f109d32d50f4abaaa70e
rafael <rafael12wq@gmail.com>
2022-01-24 15:39:24 -0300

    Create comment resource

---
+++ db/migrate/20220124182002_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.belongs_to :user, foreign_key: true
+      t.references :commentable, polymorphic: true
+      t.text :description, null: false
+      t.timestamps
+    end
+  end
+end
