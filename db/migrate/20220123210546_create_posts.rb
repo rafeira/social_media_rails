40780bdabd80d6fb82f2589a69345dacec971f78
rafael <rafael12wq@gmail.com>
2022-01-24 15:38:38 -0300

    Sign foreign key and make fields non nullable

--- db/migrate/20220123210546_create_posts.rb
+++ db/migrate/20220123210546_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.belongs_to :users
-      t.string :title
-      t.text :description
+      t.belongs_to :user, foreign_key: true
+      t.string :title, null: false
+      t.text :description, null: false
       t.timestamps
     end
   end
