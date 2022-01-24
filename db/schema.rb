3825d113395330cac3d2abef875092ca208c065f
rafael <rafael12wq@gmail.com>
2022-01-24 13:47:05 -0300

    Create post resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_145510) do
+ActiveRecord::Schema.define(version: 2022_01_23_210546) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "users_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
