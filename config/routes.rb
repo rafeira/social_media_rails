6e3575c09b7669ae05ac502794fe1f0dd98f13ef
rafael <rafael12wq@gmail.com>
2022-01-25 14:00:42 -0300

    Create likes resource routes from post resource route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root to: "posts#index"
+  resources :posts do
+    resources :likes, shallow: true
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
