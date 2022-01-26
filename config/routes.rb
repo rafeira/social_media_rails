c772d67ec9ee1e4e76cb4df70e795c741153b2eb
rafael <rafael12wq@gmail.com>
2022-01-26 13:40:16 -0300

    Create nested route between posts and comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :likes, shallow: true
   end
+  resources :posts do
+    resources :comments, shallow: true
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
