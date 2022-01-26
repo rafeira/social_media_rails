b3c85d82dd90d65f1403631626de49919d3a8b0f
rafael <rafael12wq@gmail.com>
2022-01-26 13:39:17 -0300

    Create comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+    before_action :get_post, only: [:new]
+    def new
+        @comment = Comment.new
+    end
+    def create
+        @post.comments.build(params.require(:comment)).save
+    end
+    private
+        def get_post
+        @post = Post.find(params[:post_id])
+        end
 end
