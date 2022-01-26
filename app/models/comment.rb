ef75d78322b02dd96176fa33afa7a01aa1462a44
rafael <rafael12wq@gmail.com>
2022-01-26 14:25:25 -0300

    Create association with user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :commentable, :polymorphic => true
-
+    belongs_to :user
     has_many :comments, :as => :commentable
     has_many :likes, :as => :likeable
 end
