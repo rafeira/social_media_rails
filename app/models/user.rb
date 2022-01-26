aa9eb307e5faad93445bdffe90b87762144d8d59
Rafael Vieira <rafael12wq@gmail.com>
2022-01-26 18:26:28 -0300

    Add friends association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_and_belongs_to_many :friends, class_name: "User", join_table: "user_connections",
+  foreign_key: "solicitor_user_id",
+  association_foreign_key: "requested_user_id"
   has_many :posts
   has_many :comments
   has_many :likes
