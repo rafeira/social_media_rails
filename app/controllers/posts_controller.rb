7bfaf4cb2b6c438042a95af67318c1d677fc048c
Rafael Vieira <rafael12wq@gmail.com>
2022-01-26 23:28:12 -0300

    Create new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,15 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all      
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post = Post.new(params.require(:post).permit(:title, :description))
+        post.user = current_user
+        post.save!
+        redirect_to posts_path
+    end
 end
