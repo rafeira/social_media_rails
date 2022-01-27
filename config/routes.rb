4b9b50e5d9dd9fa444bad0e63b94507d68e03d5b
rafael <rafael12wq@gmail.com>
2022-01-27 09:37:51 -0300

    Create routes to friends

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, shallow: true
   end
+  get 'friends', action: :index, controller: 'friends'
+  post 'friends/:friend_id', action: :attach, controller: 'friends'
+  delete 'friends/:friend_id', action: :detach, controller: 'friends'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
