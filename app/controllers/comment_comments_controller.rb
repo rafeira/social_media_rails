46b81435eee4dacba0c379fc94f2f00f1142672c
Rafael Vieira <rafael12wq@gmail.com>
2022-01-30 12:33:56 -0300

    Change comment related to comment_parent

--- app/controllers/comment_comments_controller.rb
+++ app/controllers/comment_comments_controller.rb
@@ -2,22 +2,24 @@ class CommentCommentsController < ApplicationController
     before_action :get_comment
 
     def index
-        @comments = @comment.comments
+        @comments = @comment_parent.comments
+        render 'comments/comment_comments/index'
     end
 
     def new
         @comment = Comment.new
+        render 'comments/comment_comments/new'
     end
 
     def create
-        comment = @comment.comments.build(params.require(:comment).permit(:description))
+        comment = @comment_parent.comments.build(params.require(:comment).permit(:description))
         comment.user = current_user
         comment.save
-        redirect_to comments_path
+        redirect_to comment_comments_path(comment_id: @comment_parent.id)
     end
 
     private
         def get_comment
-            @comment = comment.find(params[:comment_id])
+            @comment_parent = Comment.find(params[:comment_id])
         end
 end
