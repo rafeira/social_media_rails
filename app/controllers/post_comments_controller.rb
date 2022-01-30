b0c98e4bc4ffee01180c5e9dc137cb8d8bfc1053
Rafael Vieira <rafael12wq@gmail.com>
2022-01-30 12:36:00 -0300

    Change commentable name to post

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,18 +2,15 @@ class PostCommentsController < ApplicationController
     before_action :get_post
 
     def index
-        @comments = @commentable.comments
-        render 'comments/index'
+        @comments = @post.comments
+        render 'posts/comments/index'
     end
     def new
         @comment = Comment.new
-        render 'comments/new'
+        render 'posts/comments/new'
     end
     def create
-        
-        binding.pry
-        
-        comment = @commentable.comments.build(params.require(:comment).permit(:description))
+        comment = @post.comments.build(params.require(:comment).permit(:description))
         comment.user = current_user
         comment.save
         redirect_to post_comments_path post_id: @post.id
@@ -21,6 +18,6 @@ class PostCommentsController < ApplicationController
 
     private
         def get_post
-            @commentable = Post.find(params[:post_id])
+            @post = Post.find(params[:post_id])
         end
 end
