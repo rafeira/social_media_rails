5aeca0d275508561c15ba8645bc2db8c636a25bf
Rafael Vieira <rafael12wq@gmail.com>
2022-02-08 21:30:04 -0300

    Add stimulus-remote-rails package

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import ContentLoader from "stimulus-content-loader"
 
 const application = Application.start()
-
+application.register("content-loader", ContentLoader)
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
