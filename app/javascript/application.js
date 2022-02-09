370e78a82db28a71efa42feb994e271706e3f8ca
Rafael Vieira <rafael12wq@gmail.com>
2022-02-09 20:38:08 -0300

    Create class to test stimulus remove rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "jquery-ujs"
+
+
+import Remote from "stimulus-remote-rails"
+
+export default class extends Remote {
+  connect() {
+    super.connect()
+    console.log("Do what you want here.")
+  }
+}
