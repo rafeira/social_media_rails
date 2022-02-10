ba96ba947a8e710615a3af60b66da2fb5bbd15c2
Rafael Vieira <rafael12wq@gmail.com>
2022-02-10 20:52:34 -0300

    Remove unnecessary imports

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import ContentLoader from "stimulus-content-loader"
 
 const application = Application.start()
-application.register("content-loader", ContentLoader)
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
