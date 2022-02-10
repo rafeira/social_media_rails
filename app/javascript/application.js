875a116c303f2f8a65b9c2910aca0a1696b1b83b
Rafael Vieira <rafael12wq@gmail.com>
2022-02-10 20:51:33 -0300

    Remove unnecessary code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "jquery-ujs"
-
-
-import Remote from "stimulus-remote-rails"
-
-export default class extends Remote {
-  connect() {
-    super.connect()
-    console.log("Do what you want here.")
-  }
-}
