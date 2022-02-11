bdf6619a8bfcb686115b8ad8c9e89cfa6baa9ba6
Rafael Vieira <rafael12wq@gmail.com>
2022-02-10 22:33:21 -0300

    Implement button event

--- app/javascript/controllers/remote_controller.js
+++ app/javascript/controllers/remote_controller.js
@@ -3,6 +3,18 @@ import Remote from "stimulus-remote-rails"
 export default class extends Remote {
   connect() {
     super.connect()
-    console.log("Do what you want here.")
+    console.log(this)
+    this.element.addEventListener('click', (event) => {
+        
+        if (this.element.textContent == "Descurtir"){
+            this.element.classList.remove('is-danger')
+            this.element.classList.add('is-success')
+            this.element.textContent = "Curtir"
+        } else {
+            this.element.textContent = "Descurtir"
+            this.element.classList.remove('is-success')
+            this.element.classList.add('is-danger')
+        }
+    })
   }
 }
