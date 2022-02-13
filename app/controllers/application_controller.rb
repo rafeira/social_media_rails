70f020cde8de98e0325e12acbf3d9a42e1a3c440
Rafael Vieira <rafael12wq@gmail.com>
2022-02-13 07:19:35 -0300

    Create set_locale action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :set_locale
+
+    def set_locale
+        if params[:locale]
+            cookies[:locale] = params[:locale]
+        end
+        if cookies[:locale]
+            if I18n.locale != cookies
+                I18n.locale = cookies[:locale]
+            end
+        end
+    end
 end
