8decb7f715b4067f6a9898df6f1b32dfef0aa661
rafael <rafael12wq@gmail.com>
2022-02-17 10:16:07 -0300

    Separate like/dislike login in two routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,8 @@
 class LikesController < ApplicationController
   before_action :get_likeable
   def create
-    like = @likeable.likes.where(user: current_user).last
-    if like
-      like.destroy!
-    else
-      @likeable.likes.build(user: current_user).save!
-    end
-    
-    
     if @likeable.class == Post
+      @likeable.likes.build(user: current_user).save!
     elsif @likeable.class == Comment
       if @likeable.commentable.class == Post
         redirect_to post_comments_path(post_id: @likeable.commentable.id)
@@ -17,7 +10,10 @@ class LikesController < ApplicationController
         redirect_to comment_comments_path(comment_id: @likeable.commentable.id)
       end
     end
-
+  end
+  def destroy
+    like = @likeable.likes.where(user: current_user).last
+    like.destroy! if like
   end
   private
     def get_likeable
