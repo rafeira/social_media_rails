cc9c1686b6244fa0d256fecccc1400a6fd953dd0
Rafael Vieira <rafael12wq@gmail.com>
2022-03-05 20:10:51 -0300

    Pin jquery and jquery_ujs

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/d
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-remote-rails", to: "https://ga.jspm.io/npm:stimulus-remote-rails@4.0.0/dist/stimulus-remote-rails.es.js"
+pin "jquery", to: "jquery.js"
+pin "jquery_ujs", to: "jquery_ujs.js"
