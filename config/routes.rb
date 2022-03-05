8bc62fc390989f766551a6ccd10627020b98437f
Rafael Vieira <rafael12wq@gmail.com>
2022-03-05 20:18:02 -0300

    Change route all users follow helpers names

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
     get 'comments/new', action: :new, controller: 'comment_comments'
   end
   get 'all_users', action: :index, controller: 'all_users'
-  post 'all_users/:user_id', action: :attach, controller: 'all_users'
-  delete 'all_users/:user_id', action: :detach, controller: 'all_users'
+  post 'all_users/:user_id', action: :attach, controller: 'all_users', as: "follow"
+  delete 'all_users/:user_id', action: :detach, controller: 'all_users', as: "unfollow"
   get 'all_users/:followers', action: :all_followers, controller: 'all_users'
   get 'all_users/:following', action: :all_following, controller: 'all_users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
