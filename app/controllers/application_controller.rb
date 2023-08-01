79d600ede4e4725b4d49bc2550200202742b6d5e
rafeira <rafael12wq@gmail.com>
2023-08-01 19:42:56 -0300

    Refactor set_locate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :set_locale
+	before_action :authenticate_user!
+	before_action :set_locale
 
-    def set_locale
-        if params[:locale]
-            cookies[:locale] = params[:locale]
-        end
-        if cookies[:locale]
-            if I18n.locale != cookies
-                I18n.locale = cookies[:locale]
-            end
-        end
-    end
+	def set_locale
+		cookies[:locale] = params[:locale] if params[:locale]
+		I18n.locale = cookies[:locale] if I18n.locale != cookies[:locale]
+	end
 end
