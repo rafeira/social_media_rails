f2a3e018bcae22353fb877e4f8bed42893a38c2c
rafeira <rafael12wq@gmail.com>
2023-07-31 23:15:49 -0300

    Add first_name and last_name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :first_name, :last_name, presence: true, on: :create
   has_and_belongs_to_many :friends, class_name: "User", join_table: "user_connections",
   foreign_key: "solicitor_user_id",
   association_foreign_key: "requested_user_id"
