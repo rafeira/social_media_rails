a15b8608c8c3d2f74f1eb473264d9a69f8d6e4a8
rafeira <rafael12wq@gmail.com>
2023-08-01 21:01:24 -0300

    Force user to update data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 	before_action :set_locale
+	before_action :verify_user_data
 
 	def set_locale
 		cookies[:locale] = params[:locale] if params[:locale]
 		I18n.locale = cookies[:locale] if I18n.locale != cookies[:locale]
+	end
+
+	def verify_user_data
+		redirect_to users_profile_add_name_path if current_user.pending_data?
 	end
 end
--- app/controllers/users/profile/update_data_controller.rb
+++ app/controllers/users/profile/update_data_controller.rb
@@ -1,7 +1,24 @@
 module Users
   module Profile
     class UpdateDataController < ::ApplicationController
-      def add_name; end
+      skip_before_action :verify_user_data, only: [:add_name]
+      before_action :set_user
+      def add_name
+      end
+      def update
+        if @user.update(users_params)
+          redirect_to root_path
+        else
+          redirect_to users_profile_add_name_path
+        end
+      end
+      private
+      def set_user
+        @user = current_user
+      end
+      def users_params
+        params.require(:user).permit(:first_name, :last_name)
+      end
     end
   end
 end
