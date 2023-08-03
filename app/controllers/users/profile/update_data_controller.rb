a1594bbd7b368d6b8926abce822e64f5471b2edd
rafeira <rafael12wq@gmail.com>
2023-08-03 20:25:31 -0300

    Pass errors by flash

--- app/controllers/users/profile/update_data_controller.rb
+++ app/controllers/users/profile/update_data_controller.rb
@@ -1,15 +1,16 @@
 module Users
   module Profile
     class UpdateDataController < ::ApplicationController
-      skip_before_action :verify_user_data, only: [:add_name]
+      skip_before_action :verify_user_data, only: [:add_name, :update]
       before_action :set_user
+      before_action :set_errors, only: [:add_name]
       def add_name
       end
       def update
         if @user.update(users_params)
           redirect_to root_path
         else
-          redirect_to users_profile_add_name_path
+          redirect_to users_profile_add_name_path, flash: {errors: @user.errors.full_messages}
         end
       end
       private
@@ -18,6 +19,10 @@ module Users
       end
       def users_params
         params.require(:user).permit(:first_name, :last_name)
+      end
+
+      def set_errors
+        @errors = flash[:errors] || []
       end
     end
   end
