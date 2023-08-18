943039984fd57ef083697dc9e3ea25a186463547
rafeira <rafael12wq@gmail.com>
2023-08-17 21:48:02 -0300

    Configure test framework inside generators method block

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,12 @@ Bundler.require(*Rails.groups)
 module SocialMediaRails
   class Application < Rails::Application
     config.load_defaults 7.0
-    g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false
+    config.generators do |g|
+      g.test_framework :rspec,
+          fixtures: false,
+          view_specs: false,
+          helper_specs: false,
+          routing_specs: false
+    end
   end
 end
