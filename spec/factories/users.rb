ad78d3228e2f0da7828f511993e1cd6c67642a79
rafeira <rafael12wq@gmail.com>
2023-08-18 20:31:23 -0300

    Implement user factory using FFaker gem

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    password { FFaker::Internet.password(6, 10) }
   end
 end
