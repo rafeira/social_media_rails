5fd918fd7b0fa79d2f58e308285587eb1cb9bbad
rafeira <rafael12wq@gmail.com>
2023-08-20 12:58:52 -0300

    Load current user association to avoid n + 1

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
 	before_action :filter_users, :set_users, only: [:search]
 	before_action :set_user, only: [:follow, :unfollow]
+	before_action :load_current_user, only: [:search]
 	def search; end
 
 	def follow; end
@@ -18,5 +19,9 @@ class FriendshipsController < ApplicationController
 
 	def set_user
 		@user = User.find(params[:id])
+	end
+
+	def load_current_user
+		@current_user = User.includes(user_connections: :requested).find(current_user.id)
 	end
 end
