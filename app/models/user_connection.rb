221343c50a788eefbaad95767cbfb0157aee0913
rafeira <rafael12wq@gmail.com>
2023-08-19 22:57:26 -0300

    Set user connection relations

--- app/models/user_connection.rb
+++ app/models/user_connection.rb
@@ -1,2 +1,4 @@
 class UserConnection < ApplicationRecord
+  belongs_to :requester, foreign_key: :solicitor_user_id, class_name: 'User'
+  belongs_to :requested, foreign_key: :requested_user_id, class_name: 'User'
 end
