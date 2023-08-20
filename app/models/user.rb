084222517bd5bdf726eebe7ae9acfda3723137bf
rafeira <rafael12wq@gmail.com>
2023-08-19 23:54:11 -0300

    Replace has_and_belongs_to_many friends relation with has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, :last_name, presence: true, on: :create
-  has_and_belongs_to_many :friends, class_name: "User", join_table: "user_connections",
-  foreign_key: "solicitor_user_id",
-  association_foreign_key: "requested_user_id"
+  has_many :user_connections, foreign_key: :solicitor_user_id
+  has_many :following, through: :user_connections, source: :requested
+  has_many :followers, through: :user_connections, source: :requester
   has_many :posts
   has_many :comments
   has_many :likes
   has_many :friends_posts, through: :friends, source: :posts
+
+  validates :first_name, :last_name, presence: true, on: :create
 
   def pending_data?
     first_name.blank? || last_name.blank?
