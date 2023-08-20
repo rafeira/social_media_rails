6c0dbbbfa97bce950e57ce464a183e0539d9b972
rafeira <rafael12wq@gmail.com>
2023-08-19 23:52:28 -0300

    Fix posts controller indentation and fix index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,23 @@
 class PostsController < ApplicationController
-    def index
-        @posts = current_user.friends_posts.eager_load(:user, :comments, likes: :user)
-    end
+	before_action :set_posts, only: [:index]
+	def index; end
 
-    def new
-        @post = Post.new
-    end
+	def new
+		@post = Post.new
+	end
 
-    def create
-        current_user.posts.build(post_params).save!
-        redirect_to posts_path
-    end
-    private
-        def post_params
-            params.require(:post).permit(:title, :description)
-        end
+	def create
+		current_user.posts.build(post_params).save!
+		redirect_to posts_path
+	end
+	private
+		def post_params
+			params.require(:post).permit(:title, :description)
+		end
+
+		def set_posts
+			following = current_user.following.includes(posts: [ :user, :comments, { likes: :user } ])
+			@posts = following.map(&:posts).flatten(1)
+		end
 
 end
