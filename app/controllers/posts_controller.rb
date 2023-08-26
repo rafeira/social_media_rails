1ce4f5fc6cc15550c8f48f7a96d3ff1d7eec8100
rafeira <rafael12wq@gmail.com>
2023-08-26 19:19:20 -0300

    Implement like and dislike actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@
 
 class PostsController < ApplicationController
   before_action :set_posts, only: [:index]
+  before_action :set_post, :load_current_user, only: %i[like dislike]
   def index; end
 
   def new
@@ -13,6 +14,20 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def like
+    return unless @current_user.like(@post)
+
+    @post.broadcast_update(partial: 'posts/post',
+                           locals: { post: @post, logged_user: @current_user })
+  end
+
+  def dislike
+    return unless @current_user.dislike(@post)
+
+    @post.broadcast_update(partial: 'posts/post',
+                           locals: { post: @post, logged_user: @current_user })
+  end
+
   private
 
   def post_params
@@ -22,5 +37,13 @@ class PostsController < ApplicationController
   def set_posts
     following = current_user.following.includes(posts: [:user, :comments, { likes: :user }])
     @posts = following.map(&:posts).flatten(1)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def load_current_user
+    @current_user = User.includes(likes: :likeable).find(current_user.id)
   end
 end
