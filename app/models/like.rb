1cd9417b78fcf087f1134b8e3fc2ad292388f7fa
rafeira <rafael12wq@gmail.com>
2023-08-26 19:18:59 -0300

    Fix uniqueness validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,5 +3,5 @@
 class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
   belongs_to :user
-  validates :user, uniqueness: { scope: [:likeable], message: 'Combination already taken' }
+  validates :user, uniqueness: { scope: %i[likeable_type likeable_id], message: 'Combination already taken' }
 end
