43c132e1ff3417fe81d15ef72bd2589d98a0a5c0
rafeira <rafael12wq@gmail.com>
2023-09-14 21:37:07 -0300

    Set post_with_likes factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
   factory :post do
+    transient do
+      like_quantity { 1 }
+    end
+    trait :with_likes do
+      after(:create) do |post, evaluator|
+        create_list(:like, evaluator.like_quantity, likeable: post)
+      end
+      after(:build) do |post, evaluator|
+        build_list(:like, evaluator.like_quantity, likeable: post)
+      end
+    end
     title { FFaker::Lorem.sentence }
     description { FFaker::Lorem.paragraphs.join("\n\n") }
     user { association(:user) }
+    factory :post_with_likes, traits: [:with_likes]
   end
 end
