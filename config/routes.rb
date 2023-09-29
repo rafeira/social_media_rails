5c255f700eb8ae3f2a53a9454e7390263e5ff840
rafeira <rafael12wq@gmail.com>
2023-09-28 22:01:08 -0300

    Remove unnecessary resources

--- config/routes.rb
+++ config/routes.rb
@@ -9,32 +9,18 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :posts do
-    post 'likes', action: :create, controller: 'likes'
-    delete 'likes', action: :destroy, controller: 'likes'
-
-    post 'comments', action: :create, controller: 'post_comments'
-    delete 'comments/:comment_id', action: :destroy, controller: 'post_comments'
-
-    get 'comments', action: :index, controller: 'post_comments'
-    get 'comments/new', action: :new, controller: 'post_comments'
     post 'like', to: 'posts#like', on: :member
     delete 'dislike', to: 'posts#dislike', on: :member
     patch 'add_comment', to: 'posts#add_comment', on: :member
   end
-  resources :comments do
-    post 'likes', action: :create, controller: 'likes'
-    delete 'likes/:like_id', action: :destroy, controller: 'likes'
-    post 'comments', action: :create, controller: 'comment_comments'
-    delete 'comments/:comment_id', action: :destroy, controller: 'comment_comments'
-    get 'comments', action: :index, controller: 'comment_comments'
-    get 'comments/new', action: :new, controller: 'comment_comments'
-  end
+
   namespace :users do
     namespace :profile do
       get 'add_name', to: 'update_data#add_name'
       patch 'add_name', to: 'update_data#update'
     end
   end
+
   resources :friendships, only: [] do
     get 'search', to: 'friendships#search', on: :collection
     post 'follow', to: 'friendships#follow', on: :member
